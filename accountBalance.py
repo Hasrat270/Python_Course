2f90bee8110684395c4d052708eb8da36df856bb
Hasrat <hasrat3701@gmail.com>
2026-01-30 11:12:43 +0500

    Added readme

--- accountBalance.py
+++ accountBalance.py
@@ -3,7 +3,3 @@ depositBalance = input("Enter deposit amount: ")
 bankBalance += int(depositBalance)
 print("New bank balance is:", bankBalance)
 
-tankCapacity = 500
-waterUsed = input("Enter water used in liters: ")
-tankCapacity -= int(waterUsed)
-print(tankCapacity, "liters of water remaining in the tank.")
---
+++ tankCapacity.py
@@ -0,0 +1,4 @@
+tankCapacity = 500
+waterUsed = input("Enter water used in liters: ")
+tankCapacity -= int(waterUsed)
+print(tankCapacity, "liters of water remaining in the tank.")
